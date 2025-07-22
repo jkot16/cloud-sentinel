2381a06aeb8bb2b5523bb2ae2cb060071a793b85
jkot16 <kotjakub16@gmail.com>
2025-07-22 22:25:11 +0200

    feat: dynamic log rendering from CloudWatch

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,27 +1,68 @@
 from flask import Flask, render_template
-
+import boto3
+from datetime import datetime, timezone
+import os
 
 app = Flask(__name__)
 
+LOG_GROUP = os.getenv("LOG_GROUP", "cloud-sentinel-backup")
+LOG_STREAM = os.getenv("LOG_STREAM", "main-backup-log")
+REGION = os.getenv("AWS_REGION", "eu-central-1")
+
+logs_client = boto3.client("logs", region_name=REGION)
+
 @app.route("/status")
 def status():
+    events = []
+    files = []
 
-    events = [
-        {"timestamp": "2025-05-02 14:00", "message": "✅ Success – Uploaded"},
-        {"timestamp": "2025-05-02 13:00", "message": "❌ Error – S3 denied"},
-        {"timestamp": "2025-05-02 14:00", "message": "✅ Success – Uploaded"},
-        {"timestamp": "2025-05-02 14:00", "message": "✅ Success – Uploaded"},
-        {"timestamp": "2025-05-02 14:00", "message": "✅ Success – Uploaded"},
+    try:
+        response = logs_client.get_log_events(
+            logGroupName=LOG_GROUP,
+            logStreamName=LOG_STREAM,
+            startFromHead=True
+        )
 
-    ]
-    files = ["data_2025-05-02T14.tar.gz", "data_2025-05-02T13.tar.gz"]
-    success_count = sum(1 for e in events if e["message"].startswith("✅"))
-    failure_count = sum(1 for e in events if e["message"].startswith("❌"))
+        for e in response.get("events", []):
+            timestamp = datetime.fromtimestamp(e["timestamp"] / 1000, tz=timezone.utc)
+            message = e["message"]
+
+            events.append({
+                "timestamp": timestamp.strftime("%Y-%m-%d %H:%M"),
+                "message": message.strip()
+            })
+
+
+            if "Backup successful" in message and ".tar.gz" in message:
+                filename = message.split("Backup successful: ")[-1].strip()
+                files.append(filename)
+
+
+        today = datetime.now(timezone.utc).date()
+
+        success_count = 0
+        failure_count = 0
+
+        for e in events:
+            event_date = datetime.strptime(e["timestamp"], "%Y-%m-%d %H:%M").date()
+            if event_date == today:
+                if "Backup successful" in e["message"]:
+                    success_count += 1
+                elif "Backup failed" in e["message"]:
+                    failure_count += 1
+
+
+    except Exception as e:
+        print(f"[ERROR] Failed to fetch logs: {e}")
+        events = []
+        files = []
+        success_count = 0
+        failure_count = 0
 
     return render_template(
         "status.html",
-        events=events,
-        files=files,
+        events=events[::-1],
+        files=files[-5:],
         success_count=success_count,
         failure_count=failure_count
     )
